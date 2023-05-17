c1d2ea74a84506eff5e9215e70554529d0727100
Abigail Zhou <alz4471@nyu.edu>
2023-05-16 20:23:11 -0400

    tutorial -9

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+import Hello from './components/Hello'
 
 function App() {
   return (
     <div className="App">
-      <Greet />
+      <Greet name="Bruce" heroName="Batman">
+        <p>This is children props</p>
+      </Greet>
+      <Greet name="Clark"heroName="Superman">
+        <button>Action</button>
+      </Greet>
+      <Greet name="Diana" heroName="Wonder Woman" />
+      <Welcome name="Bruce" heroName="Batman" />
+      <Welcome name="Clark"heroName="Superman" />
+      <Welcome name="Diana" heroName="Wonder Woman" />
+      {/* <Hello /> */}
     </div>
   );
 }
