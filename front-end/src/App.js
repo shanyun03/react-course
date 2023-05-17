abe3369019b00d7b3a84feed5662e06242c5a0f4
Abigail Zhou <alz4471@nyu.edu>
2023-05-17 10:34:13 -0400

    tutorial -20

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,11 +8,25 @@ import Counter from './components/Counter'
 import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick'
 import EventBind from './components/EventBind'
+import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 function App() {
   return (
     <div className="App">
-      <EventBind />
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      <Inline />
+      {/* <Stylesheet primary={true}/> */}
+      {/* <NameList /> */}
+      {/* <UserGreeting /> */}
+      {/* <ParentComponent /> */}
+      {/* <EventBind /> */}
       {/* <ClassClick />
       <FunctionClick /> */}
       {/* <Counter /> */}
