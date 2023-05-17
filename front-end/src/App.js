cd1c09e9b080854a8bf2e3e46c587852bb778532
Abigail Zhou <alz4471@nyu.edu>
2023-05-17 08:46:42 -0400

    tutorial -14

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,11 +3,21 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
+import Message from './components/Message'
+import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick'
+import EventBind from './components/EventBind'
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce" heroName="Batman">
+      <EventBind />
+      {/* <ClassClick />
+      <FunctionClick /> */}
+      {/* <Counter /> */}
+      {/* <Message /> */}
+      {/* <Greet name="Bruce" heroName="Batman">
         <p>This is children props</p>
       </Greet>
       <Greet name="Clark"heroName="Superman">
@@ -16,7 +26,7 @@ function App() {
       <Greet name="Diana" heroName="Wonder Woman" />
       <Welcome name="Bruce" heroName="Batman" />
       <Welcome name="Clark"heroName="Superman" />
-      <Welcome name="Diana" heroName="Wonder Woman" />
+      <Welcome name="Diana" heroName="Wonder Woman" /> */}
       {/* <Hello /> */}
     </div>
   );
